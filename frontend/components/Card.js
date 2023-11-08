ad6392d617f7fdc5e0bd383ad680e684b63cb734
Mitch Hammond <mitch.b.hammond@gmail.com>
2023-11-08 09:30:10 -0700

    day 2 changes being made with styled components

--- frontend/components/Card.js
+++ frontend/components/Card.js
@@ -1,9 +1,28 @@
 import React from 'react'
 import Figure from './Figure'
+import styled from 'styled-components'
+
+const StyledCard = styled.div`
+border: 1px solid black;
+padding: 2rem;
+border-radius: 12px;
+
+h2 {
+  font-size: 2.5em;
+  margin: 0 0 1rem 0;
+}
+
+p{
+  margin: 0 0 1.5rem 0;
+  &::first-line{
+    font-size: 1.5rem;
+  } 
+}
+`
 
 export default function Card({title, text, imageURL, date}){
     return (
-      <div className='card'>
+      <StyledCard  $color="orange" className='card'>
       <h2 className='title1'>{title}</h2>
       <p>{text}</p>
       <Figure
@@ -11,7 +30,7 @@ export default function Card({title, text, imageURL, date}){
         caption={date}
         />
       
-    </div>
+    </StyledCard>
   
     )
   }
